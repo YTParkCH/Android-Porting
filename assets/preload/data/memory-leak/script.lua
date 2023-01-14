97335658f78b798f1cf048fb82404b5cc399f69a
YTParkCH <109469670+YTParkCH@users.noreply.github.com>
2023-01-15 03:07:30 +0900

    Add files via upload

---
+++ assets/preload/data/memory-leak/script.lua
@@ -0,0 +1,8 @@
+function onStepHit()
+
+    if curStep == 784 then
+        doTweenY(-0.1, 'memory_stage', 450, 120, 'linear');
+        doTweenY('whyyoudontmovedamn', 'sb', 200, 150, 'linear');
+    
+    end
+end
